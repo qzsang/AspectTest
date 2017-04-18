1999909aa5b3677efda3f356608932b892d2b26c
qzsang <631706099@qq.com>
2017-04-18 10:15:50 +0800

    调整

--- aoplib/src/main/java/com/example/aoplib/LogAspect.java
+++ aoplib/src/main/java/com/example/aoplib/LogAspect.java
@@ -1,8 +1,11 @@
 package com.example.aoplib;
 
+import android.content.Context;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
@@ -19,14 +22,24 @@ public class LogAspect {
 
     @Around("logMethod()")
     public Object weaveJoinPoint(ProceedingJoinPoint joinPoint) throws Throwable {
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        String className = methodSignature.getDeclaringType().getSimpleName();
-        String methodName = methodSignature.getName();
+//        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+//        String className = methodSignature.getDeclaringType().getSimpleName();
+//        String methodName = methodSignature.getName();
 
-        AOPLog.e("",className + "." + methodName + "();   start");
+
         Object result = joinPoint.proceed();
+        AOPLog.e("",joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName() + "();   end");
 
+//        AOPLog.e("",  joinPoint.getTarget() + "\n" +
+//                joinPoint.getSignature()+ "\n" +
+//                joinPoint.getKind() + "\n" +
+//                joinPoint.getArgs() + "\n" +
+//                joinPoint.getSourceLocation() + "\n" +
+//                joinPoint.getStaticPart() + "\n" +
+//                joinPoint.getClass() + "\n" +
+//                joinPoint.getThis()  + " \n"  );
         return result;
     }
+
 
 }
